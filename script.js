f572010172e0933146d744a9940f2e22ee8ce7c1
Avinash  Dhuriya <160625030+avinash-dhuriyaa@users.noreply.github.com>
2024-04-21 12:25:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let img = document.querySelector("#demo"); 
+let flag = 0
+function changeSrcAttribute(){
+ if( flag == 0){ img.setAttribute("src","bulb_on.png");
+ flag = 1
+}
+  else{
+   
+    img.setAttribute("src","bulb_off.png");
+    flag = 0
+  }   
+
+    } 
+    
+
+
+                                    
+ 
